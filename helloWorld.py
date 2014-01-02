85bf0203a58577829b727ade261a5bba23a38cc7
Juho Häppölä <juho.happola@iki.fi>
2014-01-02 12:47:38 +0300

    Init commit

---
+++ helloWorld.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+
+def justAFunction(a,b,c):
+    print('the input parameters are:')
+    for item in (a,b,c):
+        print(item)
+    return 1
+
+print('Hello, Chiheb.')
+
+justAFunction(1,2,'hello')
+
